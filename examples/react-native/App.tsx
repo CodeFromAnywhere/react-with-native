881762795bdc25bd02a42d9884c759f59174608b
Wijnand Karsens <wijnandworld@hotmail.com>
2021-12-11 21:26:33 +0530

    updates

--- examples/react-native/App.tsx
+++ examples/react-native/App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-
+import { Div } from "react-with-native";
 export default function App() {
   return (
     <View style={styles.container}>
