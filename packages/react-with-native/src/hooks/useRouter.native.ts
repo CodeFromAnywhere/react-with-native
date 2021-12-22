79bbe485fbb1a72ac06b75c412c8906f8e1824e3
Wijnand Karsens <wijnandworld@hotmail.com>
2021-12-22 12:26:32 +0530

    router hook

---
+++ packages/react-with-native/src/hooks/useRouter.native.ts
@@ -0,0 +1,80 @@
+import { useNavigation } from "@react-navigation/native";
+import { NextURL } from "next/dist/server/web/next-url";
+import { MittEmitter } from "next/dist/shared/lib/mitt";
+import { PrefetchOptions } from "next/dist/shared/lib/router/router";
+import { NextRouter, RouterEvent } from "next/router";
+
+type TransitionOptions = {
+  shallow?: boolean;
+  locale?: string | false;
+  scroll?: boolean;
+};
+
+const useRouter = (): NextRouter => {
+  const navigation = useNavigation();
+
+  const push = async (route: any, options: any) => {
+    //@ts-ignore
+    navigation.navigate(route, options?.query);
+    return true;
+  };
+
+  const route = "";
+
+  const replace = async (
+    url: NextURL,
+    as?: NextURL | undefined,
+    options?:
+      | {
+          shallow?: boolean;
+          locale?: string | false;
+          scroll?: boolean;
+        }
+      | undefined
+  ) => true;
+
+  const reload = () => {};
+  const back = () => navigation.goBack();
+
+  const prefetch = async (
+    url: string,
+    asPath?: string | undefined,
+    options?: PrefetchOptions | undefined
+  ) => {};
+  const events: MittEmitter<RouterEvent> = {
+    on: (type: RouterEvent, handler: () => void) => {},
+    off: () => {},
+    emit: () => {},
+  };
+
+  const beforePopState = (
+    cb: (state: {
+      url: string;
+      as: string;
+      options: TransitionOptions;
+    }) => boolean
+  ) => {};
+  return {
+    push,
+    route,
+    isLocaleDomain: true,
+    pathname: "",
+    query: {},
+    asPath: "",
+    isFallback: false,
+    basePath: "",
+    locale: "",
+    locales: [""],
+    defaultLocale: "",
+    isReady: true,
+    isPreview: false,
+    replace,
+    reload,
+    back,
+    prefetch,
+    beforePopState,
+    events,
+  };
+};
+
+export default useRouter;
--- packages/react-with-native/src/index.ts
+++ packages/react-with-native/src/index.ts
@@ -1,2 +1,3 @@
 export * from "./elements";
 export * from "./components";
+export * from "./hooks";
