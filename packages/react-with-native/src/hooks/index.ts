8a01ef9aae75586e4431606c01c9d76dc9f123df
Wijnand Karsens <wijnandworld@hotmail.com>
2021-12-24 17:46:51 +0530

    export one more type

--- packages/react-with-native/src/hooks/index.ts
+++ packages/react-with-native/src/hooks/index.ts
@@ -1,7 +1,7 @@
 import useRouter from "./useRouter";
 import useNavigation from "./useNavigation";
 import useStoreCreator from "./useStore";
-import { StoreConfig, StoreOptions } from "./useStore.type";
+import { StoreConfig, StoreOptions, Keys } from "./useStore.type";
 
 export { useStoreCreator, useNavigation, useRouter };
-export type { StoreConfig, StoreOptions };
+export type { StoreConfig, StoreOptions, Keys };
