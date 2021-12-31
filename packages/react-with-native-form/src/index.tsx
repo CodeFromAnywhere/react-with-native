fabec2a698bf6073efc3da9eeb9401524cb8f076
Wijnand Karsens <wijnandworld@hotmail.com>
2021-12-31 10:35:20 +0530

    Improvements

--- packages/react-with-native-form/src/index.tsx
+++ packages/react-with-native-form/src/index.tsx
@@ -375,6 +375,32 @@ const DefaultTitle = ({
   ) : null;
 };
 
+export function deepEqual(
+  object1: { [key: string]: any },
+  object2: { [key: string]: any }
+) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+  for (const key of keys1) {
+    const val1 = object1[key];
+    const val2 = object2[key];
+    const areObjects = isObject(val1) && isObject(val2);
+    if (
+      (areObjects && !deepEqual(val1, val2)) ||
+      (!areObjects && val1 !== val2)
+    ) {
+      return false;
+    }
+  }
+  return true;
+}
+export function isObject(object: any): object is object {
+  return object != null && typeof object === "object";
+}
+
 const DataForm = <TInputs, TState extends { [key: string]: any }>({
   fields,
   defaultValues,
@@ -411,10 +437,13 @@ const DataForm = <TInputs, TState extends { [key: string]: any }>({
   }, [fields]);
 
   const [state, setState] = useState<Partial<TState>>(defaultValues || {});
+  const [defaultValuesState, setDefaultValues] = useState<Partial<TState>>({});
 
   useEffect(() => {
-    if (defaultValues) {
+    if (defaultValues && !deepEqual(defaultValues, defaultValuesState)) {
+      console.log("defaultValues have changed");
       setState(defaultValues);
+      setDefaultValues(defaultValues);
     }
   }, [defaultValues]);
 
@@ -512,8 +541,11 @@ const DataForm = <TInputs, TState extends { [key: string]: any }>({
     submitButtonText,
   };
 
-  //pass them every render
-  withSubmitProps?.(submitProps);
+  console.log("Rendering Form");
+
+  useEffect(() => {
+    withSubmitProps?.(submitProps);
+  }, [loading, available, submitButtonText]);
 
   const Title = renderTitle || DefaultTitle;
 
@@ -579,6 +611,8 @@ const DataForm = <TInputs, TState extends { [key: string]: any }>({
             }
 
             setState({ ...state, ...newState });
+
+            withSubmitProps?.(submitProps);
           };
 
           const uniqueId = `${id || ""}.${field.field}`;
