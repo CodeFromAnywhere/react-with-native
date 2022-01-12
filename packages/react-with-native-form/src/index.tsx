e559e50265bf5c5859985158da9e4a8025278c2a
Wijnand Karsens <wijnandworld@hotmail.com>
2022-01-12 08:10:42 +0530

    remove logs, small fix

--- packages/react-with-native-form/src/index.tsx
+++ packages/react-with-native-form/src/index.tsx
@@ -642,7 +642,7 @@ const DataForm = <TInputs, TState extends { [key: string]: any }>({
   };
 
   useEffect(() => {
-    console.log({ withSubmitProps: submitProps });
+    //console.log({ withSubmitProps: submitProps });
     withSubmitProps?.(submitProps);
   }, [loading, available, submitButtonText, submitButtonColor, firstErrorRef]);
 
@@ -657,7 +657,7 @@ const DataForm = <TInputs, TState extends { [key: string]: any }>({
         disabled={loading}
         className={`${
           available
-            ? `${submitButtonColor ? "bg-green-500" : submitButtonColor}`
+            ? `${submitButtonColor ? submitButtonColor : "bg-green-500"}`
             : "bg-gray-300"
         }  inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
         onClick={onClickSubmit}
