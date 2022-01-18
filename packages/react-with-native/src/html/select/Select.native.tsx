9290786a38c09d1680d8ad6813aff56823e38773
rajeev rajchal <rar@trainingtube.com>
2022-01-18 17:56:16 +0545

    added select

---
+++ packages/react-with-native/src/html/select/Select.native.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { Picker } from "react-native";
+import tailwind from "tailwind-rn";
+import { SelectOptionType, SelectType } from "./Select.type";
+
+const Select = ({ native, options, ...props }: SelectType) => {
+  const { style } = native || {};
+  const tailwindStyle = props.className ? tailwind(props.className) : {};
+  return (
+    <Picker style={[{ fontWeight: "bold" }, tailwindStyle, style]}>
+      {(options || []).map((option: SelectOptionType) => {
+        <Picker.Item
+          value={String(option.value)}
+          label={String(option.label)}
+        />;
+      })}
+    </Picker>
+  );
+};
+
+export default Select;
--- packages/react-with-native/src/react-native/index.ts
+++ packages/react-with-native/src/react-native/index.ts
@@ -1,7 +1,7 @@
-import Pressable from "./Pressable";
-import Text from "./Text";
-import { PressableType } from "./Pressable.type";
-import { TextType } from "./Text.type";
+import Pressable from "./pressable/Pressable";
+import Text from "./text/Text";
+import { PressableType } from "./pressable/Pressable.type";
+import { TextType } from "./text/Text.type";
 export { Pressable, Text };
 
 export type { PressableType, TextType };
