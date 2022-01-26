a94fcf4b53bd3b960072f5d88661d44497b2a637
Wijnand Karsens <wijnandworld@hotmail.com>
2022-01-26 12:39:04 +0530

    Improvements

--- packages/react-with-native/src/html/a/A.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { AType } from "./A.type";
-
-const A = ({ native, ...props }: AType) => {
-  return <a {...props} />;
-};
-
-export default A;
--- packages/react-with-native/src/html/button/Button.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { ButtonType } from "./Button.type";
-
-const Button = ({ native, ...props }: ButtonType) => {
-  return <button {...props} />;
-};
-
-export default Button;
