f51669cee5b01fc3d8311ff43ba360f31161a8e8
Code From Anywhere <97664551+CodeFromAnywhere@users.noreply.github.com>
2022-01-26 12:37:46 +0530

    Delete packages/react-with-native/src/html/Button directory

--- packages/react-with-native/src/html/Button/Button.type.ts
+++
@@ -1,9 +0,0 @@
-import { DetailedHTMLProps, ButtonHTMLAttributes } from "react";
-import type { ButtonProps } from "react-native";
-
-export type ButtonType = DetailedHTMLProps<
-  ButtonHTMLAttributes<HTMLButtonElement>,
-  HTMLButtonElement
-> & {
-  native?: ButtonProps;
-};
