21142a091d3b4b513520d66e39962707cfd46a8a
CodeFromAnywhere <info@codefromanywhere.com>
2022-02-05 16:19:41 +0530

    Improvements

--- packages/react-with-native/src/util/util.tsx
+++ packages/react-with-native/src/util/util.tsx
@@ -3,5 +3,6 @@ import Text from "../react-native/text/Text";
 
 // NB: figure out a way to do the styles of the text right.
 export const wrapInTextIfNeeded = (children: any) => {
+  console.log({ children, type: typeof children });
   return typeof children === "string" ? <Text>{children}</Text> : children;
 };
