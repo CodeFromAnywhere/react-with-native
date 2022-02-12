86b11b6ae7b17a566d22e1ee1ff243ed266892f4
CodeFromAnywhere <info@codefromanywhere.com>
2022-02-12 12:47:04 +0530

    Improvements

--- packages/react-with-native-store/src/storage.native.ts
+++ packages/react-with-native-store/src/storage.native.ts
@@ -1,8 +1,11 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 export const getItem = async (key: string) => {
+  console.log("reading key ", key);
   try {
     const item = await AsyncStorage.getItem(key);
-    return item ? JSON.parse(item) : undefined;
+    const value = item ? JSON.parse(item) : undefined;
+    console.log(`we got ${item}`);
+    return value;
   } catch (e) {
     // error reading value
     console.warn(
@@ -13,8 +16,13 @@ export const getItem = async (key: string) => {
 };
 
 export const setItem = async (key: string, value: any) => {
+  console.log("AsyncStorage setting item");
+  const stringValue = JSON.stringify(value);
   try {
-    await AsyncStorage.setItem(key, JSON.stringify(value));
+    await AsyncStorage.setItem(key, stringValue, (error) => {
+      console.warn("something went wrong with asyncStorage", error);
+    });
+    console.log(`we set ${key} to ${stringValue}`);
   } catch (e) {
     // saving error
     console.warn(
