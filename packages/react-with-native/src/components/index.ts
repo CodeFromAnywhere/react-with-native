f818aae0284e9905aca03afe1bff20873d709199
Guilherme Reis <grrbm2@gmail.com>
2022-04-26 20:13:41 -0300

    added scrollable div component

--- packages/react-with-native/src/components/index.ts
+++ packages/react-with-native/src/components/index.ts
@@ -2,6 +2,14 @@ import { ToggleType } from "./Toggle/Toggle.type";
 
 import Toggle from "./Toggle/Toggle";
 
+import { ScrollableDivType } from "./ScrollableDiv/ScrollableDiv.type";
+
+import ScrollableDiv from "./ScrollableDiv/ScrollableDiv";
+
 export { Toggle };
 
 export type { ToggleType };
+
+export { ScrollableDiv };
+
+export type { ScrollableDivType };
