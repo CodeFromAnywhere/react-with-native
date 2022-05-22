0e0bb83e1fc8c21cf4a6092d26085be130c10483
Wijnand Karsens <info@codefromanywhere.com>
2022-05-22 18:44:12 +0200

    fix hopefully

--- apps/web/.next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/utilities":["static\u002Fchunks\u002Fpages\u002Futilities.js"],sortedPages:["\u002F_app","\u002F_error","\u002Futilities"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
