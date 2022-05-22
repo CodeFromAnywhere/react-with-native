85ba13e1fc56b6a0d4bcf0d46bb7a9dfb1c6b50d
Wijnand Karsens <info@codefromanywhere.com>
2022-05-22 20:01:26 +0200

    Improvements

--- apps/web/.next/static/chunks/pages/form-dc366e188f8d25bf.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[894],{8954:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/form",function(){return _(2866)}])},2866:function(n,u,_){"use strict";_.r(u);var r=_(865);u.default=r.iY.form}},function(n){n.O(0,[774,888,179],(function(){return u=8954,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
